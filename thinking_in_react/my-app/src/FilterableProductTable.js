ae8859c1ac821cf3ad7c4acf3f7a8a77209fc4f3
Quang Nguyen <quang.nguyen@nfq.asia>
2022-01-27 00:39:51 +0700

    https://create-react-app.dev/docs/

---
+++ thinking_in_react/my-app/src/FilterableProductTable.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class FilterableProductTable extends Component {
+   render() {
+       return <div>FilterableProductTable</div>
+   }
+}
+export default FilterableProductTable;
---
+++ thinking_in_react/readme.txt
@@ -0,0 +1,6 @@
+1- Install Node
+2- Install Npm
+3- Crate react app
+npm init react-app my-app
+4- cd my-app
+5 - npm start
